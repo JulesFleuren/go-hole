47bd391484b5b099888ef57c792dec14e5a99ce5
Jules <julesfleuren@hotmail.com>
2024-02-13 14:34:10 +0100

    removed local blacklists

--- blacklist.go
+++ blacklist.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"io"
-	"log"
-	"os"
 	"sort"
 	"strings"
 	"time"
@@ -73,85 +68,4 @@ func (blacklist *Blacklist) Contains(domain string) bool {
 
 	// if here, the domain is not present at all
 	return false
-}
-
-// LoadBlacklistOrFail loads the blacklist from the given file
-// and panics if there are errors with loading the list.
-func LoadBlacklistOrFail(path string) *Blacklist {
-	domains, err := LoadBlacklist(path)
-	if err != nil {
-		log.Panic(err)
-	}
-	return domains
-}
-
-// LoadBlacklist loads the blacklist from the given file.
-func LoadBlacklist(path string) (*Blacklist, error) {
-
-	// open the file
-	file, errCount := os.Open(path)
-	if errCount != nil {
-		return nil, errCount
-	}
-	defer file.Close()
-
-	// count the number of blocked domains
-	lines, errCount := countLines(file)
-	if errCount != nil {
-		return nil, errCount
-	}
-
-	// allocate the data structure of optimal Size
-	blacklist := Blacklist{
-		filter: bloom.New(uint(lines)*10, 5),
-		array:  make([]string, lines),
-	}
-
-	// start again from the beginning of the file
-	_, errSeek := file.Seek(0, 0)
-	if errSeek != nil {
-		return nil, errSeek
-	}
-
-	// read the file line-by-line
-	// NB: the file MUST be ORDERED and all domains LOWER CASE!
-	reader := bufio.NewReader(file)
-	for i := 0; i < lines; i++ {
-		line, err := reader.ReadString('\n')
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		// NB: we trim spaces to eliminate the '\n' at the end
-		trimmed := strings.TrimSpace(line)
-
-		// add the domain to both the bloom filter and the ordered array
-		blacklist.filter.AddString(trimmed)
-		blacklist.array[i] = trimmed
-	}
-
-	return &blacklist, nil
-}
-
-// countLines count the number of lines in the given file.
-// Please note that it will move to the end of the file, so rewind is needed.
-func countLines(file io.Reader) (int, error) {
-	size := 2 * 1024
-	buffer := make([]byte, size)
-	count := 0
-	for {
-		c, err := file.Read(buffer)
-		count += bytes.Count(buffer[:c], []byte{'\n'})
-
-		switch {
-		case err == io.EOF:
-			return count, nil
-
-		case err != nil:
-			return count, err
-		}
-	}
 }
