94a6febdff0bf5e8b6765548d4f6aa44779fbc7b
JulesFleuren <95241984+JulesFleuren@users.noreply.github.com>
2024-04-05 17:18:58 +0200

    added AAAA override

--- override.go
+++ override.go
@@ -8,6 +8,7 @@ import (
 )
 
 type Override struct {
+	Type   string `json:"Type"`
 	Domain string `json:"Domain"`
 	Ip     string `json:"Ip"`
 }
@@ -16,31 +17,70 @@ func addOverridesToCache(config *Config, cache *Cache) {
 	for _, override := range config.Overrides {
 		// create a mock request for this domain
 		req := new(dns.Msg)
-		req.SetQuestion(dns.Fqdn(override.Domain), dns.TypeA)
 
+		// create a response that will be added to the cache
 		res := new(dns.Msg)
+
 		if override.Ip == "" {
+			if override.Type == "A" {
+				req.SetQuestion(dns.Fqdn(override.Domain), dns.TypeA)
+			} else if override.Type == "AAAA" {
+				req.SetQuestion(dns.Fqdn(override.Domain), dns.TypeAAAA)
+			} else {
+				continue
+			}
 			// create a NXDOMAIN response
 			res.SetRcode(req, dns.RcodeNameError)
+			cache.Set(&req.Question[0], res, NoExpiration)
 		} else {
-			// create a response with the right ip address
-			record := new(dns.A)
-			record.Hdr = dns.RR_Header{
-				Name:   dns.Fqdn(override.Domain),
-				Rrtype: dns.TypeA,
-				Class:  dns.ClassINET,
-				Ttl:    3600,
-			}
-			record.A = net.ParseIP(override.Ip)
-			if record.A == nil {
-				log.Printf("Not able to parse override IP: %s\n", override.Ip)
+
+			// create the answer and add it to the response
+			switch override.Type {
+			case "A":
+				recordType := dns.TypeA
+
+				record := new(dns.A)
+				record.Hdr = dns.RR_Header{
+					Name:   dns.Fqdn(override.Domain),
+					Rrtype: recordType,
+					Class:  dns.ClassINET,
+					Ttl:    3600,
+				}
+				record.A = net.ParseIP(override.Ip)
+				if record.A == nil {
+					log.Printf("Not able to parse override IP: %s\n", override.Ip)
+					continue
+				}
+
+				req.SetQuestion(dns.Fqdn(override.Domain), recordType)
+				res.SetReply(req)
+				res.Answer = []dns.RR{record}
+
+				// add record to cache with no expiration
+				cache.Set(&req.Question[0], res, NoExpiration)
+
+			case "AAAA":
+				recordType := dns.TypeAAAA
+
+				record := new(dns.AAAA)
+				record.Hdr = dns.RR_Header{
+					Name:   dns.Fqdn(override.Domain),
+					Rrtype: recordType,
+					Class:  dns.ClassINET,
+					Ttl:    3600,
+				}
+				record.AAAA = net.ParseIP(override.Ip)
+				if record.AAAA == nil {
+					log.Printf("Not able to parse override IP: %s\n", override.Ip)
+					continue
+				}
+
+			default:
+				log.Printf("Unsupported override type: %s\n", override.Type)
 				continue
 			}
 
-			res.SetReply(req)
-			res.Answer = []dns.RR{record}
 		}
-		cache.Set(&req.Question[0], res, NoExpiration)
 	}
 	log.Printf("Succesfully added %v overrides\n", cache.c.ItemCount())
 }
